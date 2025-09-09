f1188396ce84d204c79cf8ac5d27c044ab2db8ad
tauam <tauam.pinheiro@goias.gov.br>
2025-09-09 17:09:17 -0300

    criando banco de dados

---
+++ prepara_banco.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+banco = sqlite3.connect("imovel.db") #variavel para criar ou se conectar com o banco
+
+#cria a variavel cursor que vai receber o objeto banco e irá utilizar o metodo cursor que vai nos possibilitar criar tabelas e manipular o banco de dados 
+
+cursor = banco.cursor()
+
+
+# cursor.execute('''CREATE TABLE corretores (
+#                id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                nome TEXT NOT NULL,
+#                Contato TEXT NOT NULL
+#                )''')
+
+# cursor.execute('''CREATE TABLE Imoveis(
+#                id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                regiao TEXT NOT NULL, 
+#                preco INTEGER NOT NULL,
+#                quartos INTEGER, 
+#                area_total TEXT ,
+#                area_construida TEXT,
+#                area_gourmet TEXT NOT NULL CHECK(area_gourmet IN('Sim', 'Não')),
+#                Valor_entrada INTEGER,
+#                id_corretor INTEGER NOT NULL,
+#                FOREIGN KEY(id_corretor) REFERENCES corretores(id)
+#                )
+#                ''')
+
+
+cursor.execute("INSERT INTO corretores (nome, Contato) VALUES ('Lourenço Imoveis', '(62) 98428-4002')")
+cursor.execute(''' SELECT * from corretores''')
+
+print(cursor.fetchall()) #
+
+banco.commit()
--- services.py
+++ services.py
@@ -1,24 +1,4 @@
 
-# imoveis = [
-#     {
-#         "id": 1,
-#         "endereco": "Rua das Flores, 123, Bairro Jardim",
-#         "preco": "R$ 350.000,00",
-#         "link": "https://www.instagram.com/p/DNovJNPJ2_P/?img_index=1",
-#         "corretor": "Maria Silva",
-#         "telefone_corretor": "(62) 99999-1111"
-#     },
-#     {
-#         "id": 2,
-#         "endereco": "Avenida Principal, 456, Setor Central",
-#         "preco": "R$ 480.000,00",
-#         "link": "https://www.instagram.com/p/DNlJ2uLOREZ/",
-#         "corretor": "João Costa",
-#         "telefone_corretor": "(62) 98888-2222"
-#     }
-# ]
-
-
 class Imoveis:
     def __init__(self, endereco, preco,terreno_total, quartos, link, corretor, telefone_corretor):
         self.endereco = endereco
